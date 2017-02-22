6a1573fca29641301bbde128687c493fffb35fb3
greekw <xhwwc397@163.com>
2017-02-22 21:05:52 +0800

    python leetcode题

--- ch43/basic_calculator.py
+++ ch43/basic_calculator.py
@@ -5,16 +5,16 @@ __author__ = 'greek'
   基础运算器
 '''
 class Solution(object):
-    def caculate(self,s):
-        stackOp=['#']
+    def calculate(self,s):
+        stackOp=[]
         stackNum=[]
         res=''
         for i in reversed(range(len(s))):
             if s[i].isdigit():
                 res+=s[i]
                 if i==0 or not s[i-1].isdigit():
-                    res=''
                     stackNum.append(int(res[::-1]))
+                    res=''
             elif s[i]==')'or s[i] == '*' or s[i] == '/':
                 stackOp.append(s[i])
             elif s[i]=='+' or s[i]=='-':
@@ -42,4 +42,8 @@ class Solution(object):
             stackNum.append(left / right)
 
 if __name__ == "__main__":
-    Solution.caculate(object,"3+2*2")
+    s=Solution()
+    param="3+2*2+2/4"
+    print(len(param))
+    print(reversed(range(len(param))))
+    print(s.calculate(param))
